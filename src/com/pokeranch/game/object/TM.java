e4136e342fd1f1d5173e162180dcd53def350849
Genta Indra Winata <gentaindrawinata@gmail.com>
2013-04-30 13:55:11 +0700

    TM updated

--- src/com/pokeranch/game/object/TM.java
+++ src/com/pokeranch/game/object/TM.java
@@ -2,12 +2,21 @@ package com.pokeranch.game.object;
 
 public class TM extends Item{
 
+	private Skill skill;
+	
 	//ctor
 	TM(){
 		
 	}
 
-	int itemType() {
-		return 3;
+	//getter dan setter
+	public Skill getSkill() {
+		return skill;
 	}
+
+	public void setSkill(Skill skill) {
+		this.skill = skill;
+	}
+	
+	
 }
