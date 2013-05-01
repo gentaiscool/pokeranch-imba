8139db4603f02a2e9725fc46f9d1d8aab30b4fe2
Genta Indra Winata <gentaindrawinata@gmail.com>
2013-05-01 10:48:05 +0700

    Xml updated. Font Pokemon GB in assets

--- src/com/pokeranch/game/system/MainMenu.java
+++ src/com/pokeranch/game/system/MainMenu.java
@@ -2,11 +2,18 @@ package com.pokeranch.game.system;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Typeface;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,6 +22,7 @@ public class MainMenu extends Activity {
 	private ImageButton newGame, loadGame, helpGame, exitGame; 
 	private int state;
 	TextView tf;
+	private String characterName, monsterName;
 	//private Bitmap newgame;
 	//private Resources res;
 	
@@ -72,8 +80,81 @@ public class MainMenu extends Activity {
 		});
 	}
 	
+	public void onPickMonster(){
+		final RadioButton b1 = (RadioButton)findViewById(R.id.RadioButtonBulba);
+		final RadioButton b2 = (RadioButton)findViewById(R.id.RadioButtonSquir);
+		final RadioButton b3 = (RadioButton)findViewById(R.id.RadioButtonCharchar);
+		b1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				Log.d("he", "click");
+				if(b1.isChecked()){
+					b2.setChecked(false);
+					b3.setChecked(false);
+				}
+				
+			}
+		});
+		b2.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				Log.d("he", "click2a");
+				if(b2.isChecked()){
+					b1.setChecked(false);
+					b3.setChecked(false);
+				}
+			}
+		});
+		b3.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				Log.d("he", "click3");
+				if(b3.isChecked()){
+					b1.setChecked(false);
+					b2.setChecked(false);
+				}
+			}
+		});
+	}
+	
+	public void onWriteName(){
+		Typeface face = Typeface.createFromAsset(getAssets(),
+	            "fonts/Pokemon GB.ttf");
+		TextView tv = (TextView)findViewById(R.id.textViewHint);
+		final EditText et = (EditText)findViewById(R.id.editTextName);
+		Button b = (Button)findViewById(R.id.buttonContinue);
+		tv.setTypeface(face);
+		b.setTypeface(face);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(et.getText().length()!=0){
+					state = 13; // help game
+					setContentView(R.layout.main_pick_monster);
+					onPickMonster();
+				}
+				else{
+					
+				}
+			}
+		});
+	}
+	
 	public void onStory(){
+		Typeface face = Typeface.createFromAsset(getAssets(),
+	            "fonts/Pokemon GB.ttf");
 		tf = (TextView)findViewById(R.id.textViewDesc);
+		TextView tap = (TextView)findViewById(R.id.textViewTap);
+		tf.setTypeface(face);
+		tap.setTypeface(face);
+		
 		RelativeLayout rl = (RelativeLayout)findViewById(R.id.relativeLayoutStory);
 		switch(state){
 			case 1:tf.setText("Welcome to PokeRancher World!. An amazing journey is waiting you.");
@@ -87,9 +168,10 @@ public class MainMenu extends Activity {
 					case 1 :
 						tf.setText("Please introduce yourself!");
 						state = 11;
-							break;
+						break;
 					case 11 :
 						setContentView(R.layout.main_write_name);
+						onWriteName();
 						state = 12;
 						break;
 				}
