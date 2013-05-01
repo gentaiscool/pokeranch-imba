71fb2709a5e51eddcd2ce491059f36d008a2b305
Faiz Ilham <indigo.republica@gmail.com>
2013-05-01 19:11:51 +0700

    ScreenManager

--- src/com/pokeranch/game/system/IScene.java
+++ src/com/pokeranch/game/system/IScene.java
@@ -1,10 +1,13 @@
 package com.pokeranch.game.system;
 
 import android.graphics.Canvas;
+
 import com.pokeranch.game.system.MainGameView.ButtonClick;
 
 public interface IScene {
 	public void update();
 	public void draw(Canvas canvas);
-	public void getButtonInput(ButtonClick click);
+	//public void OnTouchEvent(MotionEvent e);
+	
+	public void getButtonInput(ButtonClick b);
 }
