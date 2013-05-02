d2fd0c89000f22a877805c2d6af299e6afb25060
Genta Indra Winata <gentaindrawinata@gmail.com>
2013-05-02 21:34:40 +0700

    updated

--- src/com/pokeranch/game/system/MainMenu.java
+++ src/com/pokeranch/game/system/MainMenu.java
@@ -162,8 +162,10 @@ public class MainMenu extends Activity {
 		Typeface face = Typeface.createFromAsset(getAssets(),
 	            "fonts/Pokemon GB.ttf");
 		TextView tv = (TextView)findViewById(R.id.textViewHint);
-		final EditText et = (EditText)findViewById(R.id.editTextName);
+		EditText et = (EditText)findViewById(R.id.editTextName);
 		Button b = (Button)findViewById(R.id.buttonContinue);
+		
+	
 		tv.setTypeface(face);
 		et.setTypeface(face);
 		b.setTypeface(face);
@@ -171,7 +173,7 @@ public class MainMenu extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if(et.getText().length()!=0){
+				/*if(et.getText().length()!=0){
 					state = 13; // pick monster
 					characterName = et.getText().toString();
 					setContentView(R.layout.main_pick_monster);
@@ -180,7 +182,7 @@ public class MainMenu extends Activity {
 				else{
 					AlertDialog ad = makeAndShowDialogBox("Gotcha!","You haven't type your name!");
 					ad.show();
-				}
+				}*/
 			}
 		});
 	}
--- src/com/pokeranch/game/system/PokeDex.java
+++ src/com/pokeranch/game/system/PokeDex.java
@@ -12,12 +12,15 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -36,6 +39,11 @@ public class PokeDex extends Activity{
 		TextView typeMonster = (TextView)findViewById(R.id.textViewTypeMonster);
 		TextView numMonster = (TextView)findViewById(R.id.textViewMonsterNumber);
 		TextView tv = (TextView)findViewById(R.id.textViewNamaMonster);
+		TextView hpMonster = (TextView)findViewById(R.id.textViewHPBase);
+		TextView mpMonster = (TextView)findViewById(R.id.textViewMPBase);
+		TextView attackMonster = (TextView)findViewById(R.id.textViewAttackBase);
+		TextView defenseMonster = (TextView)findViewById(R.id.textViewDefenseBase);
+		
 		ImageView monsterImage = (ImageView)findViewById(R.id.imageViewMonster);
 		
 		iv.setTypeface(face);
@@ -54,15 +62,50 @@ public class PokeDex extends Activity{
 
 		Drawable d = getImageByName(((Species) species[0]).getName().toString().toLowerCase(), this);
 		monsterImage.setImageDrawable(d);
-		tv.setText((String)((Species) species[0]).getName());
-		numMonster.setText("001");
-		typeMonster.setText((String)((Species) species[0]).getElement().getName());
 		
+		Species species1= (Species) species[0];
+		
+		tv.setText((String)species1.getName());
+		numMonster.setText("001");
+		typeMonster.setText((String)(species1).getElement().getName());
+		hpMonster.setText("HP: " + species1.getBaseStat().getHP());
+		mpMonster.setText("MP: " + species1.getBaseStat().getMP());
+		attackMonster.setText("Attack: " + species1.getBaseStat().getAttack());
+		defenseMonster.setText("Defense: " + species1.getBaseStat().getDefense());
+		
+		
+		//tableRow
 		for(int i=0; i<=species.length-1;i++){
-			tableRow = new TableRow(getBaseContext());
+			final TableRow tableRow2 = new TableRow(getBaseContext());
+			tableRow2.setMinimumHeight(40);
+			
 			textView = new TextView(getBaseContext());
-			textView.setText((String)((Species) species[i]).getName());
+			textView.setOnTouchListener(new OnTouchListener() {
+				
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					switch(event.getAction()){            
+				            case MotionEvent.ACTION_DOWN:
+				            	tableRow2.setBackgroundColor(0xFFFFFFFF); //white
+				                break;          
+				            case MotionEvent.ACTION_CANCEL:             
+				            case MotionEvent.ACTION_UP:
+				            	tableRow2.setBackgroundColor(0xFF000000); //black
+				                break;
+				    } 
+					return false;
+				}
+			});
+			int i2 = i+1;
+			if(i2>9){
+				textView.setText("0"+i2+"  "+(String)((Species) species[i]).getName());
+			}
+			else{
+				textView.setText("00"+i2+"  "+(String)((Species) species[i]).getName());
+			}
 			final Integer ii = new Integer(i);
+			textView.setTypeface(face);
 			textView.setOnClickListener(new View.OnClickListener() {
 				@SuppressLint("UseValueOf")
 				@Override
@@ -72,9 +115,15 @@ public class PokeDex extends Activity{
 					TextView typeMonster = (TextView)findViewById(R.id.textViewTypeMonster);
 					TextView numMonster = (TextView)findViewById(R.id.textViewMonsterNumber);
 					ImageView monsterImage = (ImageView)findViewById(R.id.imageViewMonster);
+					Species sp = (Species)species[ii];
+					TextView hpMonster = (TextView)findViewById(R.id.textViewHPBase);
+					TextView mpMonster = (TextView)findViewById(R.id.textViewMPBase);
+					TextView attackMonster = (TextView)findViewById(R.id.textViewAttackBase);
+					TextView defenseMonster = (TextView)findViewById(R.id.textViewDefenseBase);
+					
 					tv.setTypeface(face);
 					typeMonster.setTypeface(face);
-					Drawable d = getImageByName(((Species) species[ii]).getName().toString().toLowerCase(), PokeDex.this);
+					Drawable d = getImageByName(sp.getName().toString().toLowerCase(), PokeDex.this);
 					monsterImage.setImageDrawable(d);
 					Integer ii2 = ii+1;
 					if(ii2>9){
@@ -83,12 +132,19 @@ public class PokeDex extends Activity{
 					else{
 						numMonster.setText("00"+ii2.toString());
 					}
-					typeMonster.setText((String)((Species) species[ii]).getElement().getName());
-					tv.setText((String)((Species) species[ii]).getName());
+					
+					typeMonster.setText((String)sp.getElement().getName());
+					hpMonster.setText("HP: " + sp.getBaseStat().getHP());
+					mpMonster.setText("MP: " + sp.getBaseStat().getMP());
+					attackMonster.setText("Attack: " + sp.getBaseStat().getAttack());
+					defenseMonster.setText("Defense: " + sp.getBaseStat().getDefense());
+					
+					typeMonster.setText((String)sp.getElement().getName());
+					tv.setText((String)sp.getName());
 				}
 			});
-			tableRow.addView(textView);
-			tableLayout.addView(tableRow);
+			tableRow2.addView(textView);
+			tableLayout.addView(tableRow2);
 		}
 	}
 
