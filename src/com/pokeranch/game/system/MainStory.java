42ddfa9bd84c892b0d76e924355836ace5b3c042
Faiz Ilham <indigo.republica@gmail.com>
2013-05-10 00:45:31 +0700

    benerin punya genta

--- src/com/pokeranch/game/system/MainStory.java
+++ src/com/pokeranch/game/system/MainStory.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.Log;
 import android.view.MotionEvent;
 
@@ -16,7 +17,7 @@ public class MainStory implements IScreen{
 	
 	private Paint paint = new Paint();
 	private ScreenManager manager;
-	private BitmapButton transparentButton, oak, frame;
+	private BitmapButton oak, frame;
 	
 	private int state = 0;
 	private TextComponent textcomponent;
@@ -40,61 +41,45 @@ public class MainStory implements IScreen{
 			paint.setTypeface(BitmapManager.getInstance().getTypeface());
 			paint.setColor(Color.BLACK);	
 			
-			BitmapManager.getInstance().putImage("transparent", R.drawable.transparent, screenWidth, screenHeight);
+			
 			
 			frame = new BitmapButton(BitmapManager.getInstance().get("frame"),65,150);
 			oak = new BitmapButton(BitmapManager.getInstance().get("professoroak"),19,60);
-			transparentButton = new BitmapButton(BitmapManager.getInstance().get("transparent"),0,0);
-			transparentButton.addTouchListener(new TouchListener() {
-				
-				@Override
-				public void onTouchUp() {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void onTouchMove() {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void onTouchDown() {
-					// TODO Auto-generated method stub
-					Log.d("transparent", "tekan"+state);
-					state++;
-					
-					if(state > 1){
-						MessageManager.prompt("What is your name?", new Action() {
-							@Override
-							public void proceed(Object o) {
-								// TODO Auto-generated method stub
-								if(!o.toString().equals("")){
-									//check if the name exists before
-									if(PlayerSaveLoader.getInstance().isPlayerNotExist(o.toString())){
-										selectFrom();
-									}
-									else{
-										MessageManager.alert("Player name exist");
-									}
-									//state=-1;
-								}
-								else{
-									
-								}
-							}
-							
-							@Override
-							public void cancel() {
-								// TODO Auto-generated method stub
-								
-							}
-						}, true);
-					}
-				}
-			});
 			
+			
+		}
+		
+		private void touchGlobal(){
+			Log.d("transparent", "tekan"+state);
+			state++;
+			
+			if(state > 2){
+				MessageManager.prompt("What is your name?", new Action() {
+					@Override
+					public void proceed(Object o) {
+						// TODO Auto-generated method stub
+						if(!o.toString().equals("")){
+							//check if the name exists before
+							if(PlayerSaveLoader.getInstance().isPlayerNotExist(o.toString())){
+								selectFrom();
+							}
+							else{
+								MessageManager.alert("Player name exist");
+							}
+							//state=-1;
+						}
+						else{
+							
+						}
+					}
+					
+					@Override
+					public void cancel() {
+						// TODO Auto-generated method stub
+						
+					}
+				}, true);
+			}
 		}
 		
 		public void selectFrom(){
@@ -123,11 +108,10 @@ public class MainStory implements IScreen{
 			
 			logo.draw(canvas);*/
 		
-			transparentButton.draw(canvas);
 			oak.draw(canvas);
 			frame.draw(canvas);
 			canvas.drawText("tap to continue...", 200, 0, paint);
-			if(state == 0){
+			if(state == 1){
 				/*canvas.drawText("Welcome to PokeRancher World!. ", 77, 172, paint);
 				canvas.drawText("An amazing journey is waiting", 77, 180, paint);
 				canvas.drawText("you.", 77, 188, paint);*/
@@ -146,6 +130,11 @@ public class MainStory implements IScreen{
 			/*for(BitmapButton b : buttons){
 				b.onTouchEvent(e, mag);
 			}*/
-			transparentButton.onTouchEvent(e, magX, magY);
+			
+			final int actioncode = e.getAction() & MotionEvent.ACTION_MASK;	
+			
+			if (actioncode==MotionEvent.ACTION_UP){
+				touchGlobal();
+			}
 		}
 }
